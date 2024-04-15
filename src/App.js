1774a855e172370cbf64f6d739d4990804e20b45
Clóvis Chakrian <clovischakriandev@gmail.com>
2024-04-15 19:13:54 -0300

    Feature: adicionei a possibilidade de encerrar o jogo após 5 rodadas

--- src/App.js
+++ src/App.js
@@ -8,37 +8,58 @@ function App() {
   const [pontuacaoP2, setPontuacaoP2] = useState(0);
   const [jogadaP1, setJogadaP1] = useState(1);
   const [jogadaP2, setJogadaP2] = useState(1);
+  const [rodadas, setRodadas] = useState(5);
 
   useEffect(() => {
-    if (ativo) {
+    if (ativo && rodadas < 5) {
       if (jogadaP1 > jogadaP2) {
-        setPontuacaoP1(pontuacaoP1+1);
+        setPontuacaoP1(pontuacaoP1 + 1);
       };
 
       if (jogadaP2 > jogadaP1) {
-        setPontuacaoP2(pontuacaoP2+1);
+        setPontuacaoP2(pontuacaoP2 + 1);
       }
     }
   }, [jogadaP2]);
 
+  function resultadoJogada() {
+    if (pontuacaoP1 > pontuacaoP2) {
+      return "O jogador 1 ganhou!";
+    }
+
+    if (pontuacaoP2 > pontuacaoP1) {
+      return "O jogador 2 ganhou!";
+    }
+
+    return "Houve um empate!";
+  }
+
   function toggleAtivo() {
     setAtivo(!ativo);
+    if (!ativo) {
+      setRodadas(rodadas + 1);
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='title'>Jogo do Dado</h1>
+        <h1 className='title'>Jogo do Dado (Rodada atual: {rodadas})</h1>
+
+        {rodadas >= 5 && <h2>{resultadoJogada()}</h2>}
+
+        {rodadas >= 5 && <button className='button btnJogarNovamente' onClick={() => setRodadas(0)}>Jogar Novamente</button>}
+
 
         <div className="arena">
           <div className="player-container">
             <p>Pontuação Player 1: {pontuacaoP1}</p>
-            <Dado ativo={!ativo} toggleAtivo={toggleAtivo} setJogada={setJogadaP1} jogada={jogadaP1} />
+            <Dado ativo={!ativo} jogoAcabou={rodadas >= 5} toggleAtivo={toggleAtivo} setJogada={setJogadaP1} jogada={jogadaP1} />
           </div>
 
           <div className="player-container">
             <p>Pontuação Player 2: {pontuacaoP2}</p>
-            <Dado ativo={ativo} toggleAtivo={toggleAtivo} setJogada={setJogadaP2} jogada={jogadaP2} />
+            <Dado ativo={ativo} jogoAcabou={rodadas >= 5} toggleAtivo={toggleAtivo} setJogada={setJogadaP2} jogada={jogadaP2} />
           </div>
         </div>
       </header>
--- src/components/Dado.js
+++ src/components/Dado.js
@@ -1,4 +1,4 @@
-export function Dado({ ativo, toggleAtivo, setJogada, jogada }) {
+export function Dado({ ativo, toggleAtivo, setJogada, jogada, jogoAcabou }) {
   function geraNumero() {
     toggleAtivo();
     return (Math.random() * 5 + 1).toFixed(0);
@@ -8,7 +8,7 @@ export function Dado({ ativo, toggleAtivo, setJogada, jogada }) {
     <>
       <img className='dado-image' src={`/images/dado${jogada}.png`} alt={`dado de numeração ${jogada}`} />
       <p>última jogada: {jogada}</p>
-      <button className='button' disabled={ativo} type='button' onClick={() => setJogada(geraNumero())}>Jogar</button>
+      <button className='button' disabled={ativo || jogoAcabou} type='button' onClick={() => setJogada(geraNumero())}>Jogar</button>
     </>
   );
 };
